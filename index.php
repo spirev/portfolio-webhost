e325df7b51480425e5c0dbc8cf092e694f667ca2
spirev <prospirev@gmail.com>
2021-10-11 23:05:22 +0200

    new project architecture, now passing by an index.php and a layout instead of plain html file

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
 
-include 'index.html';
+$view = "";
+$title = "";
+$is_in_range = false;
+
+foreach ($_GET as $key => $value) {
+    $view = $key;
+}
+
+$pages = [
+    ["parcours", "Mon parcours"],
+    ["projets", "Mes projets"],
+    ["interets", "Mes centres d'intérets"],
+    ["contact", "Me contacter"]
+];
+
+foreach ($pages as $page) {
+    if (in_array($view, $page)) {
+        $is_in_range = true;
+        $title = $page[1];
+        break;
+    }
+}
+
+if ($is_in_range === false) {
+    $view = 'parcours';
+}
+
+include 'views/layout.phtml';
