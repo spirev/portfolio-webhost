266367e29bad37bc3e566aaa87bb3dacc8c2570d
spirev <prospirev@gmail.com>
2021-10-17 23:34:55 +0200

    Les images de présentation des projets changent en fonction du timer défini si la largeure de l'écran est superieur a celui d'un mobile ( 425px )

--- js/projects.js
+++ js/projects.js
@@ -1,11 +1,36 @@
 $(document).ready(function () {
 
     let window_width = window.innerWidth;
+    let img_element = $('.caroussel');
+    let caroussel_array = [
+        ['images/Ready-A-L1.png', 'images/Ready-A-L2.png', 'images/Ready-A-L3.png', 'images/Ready-A-L4.png'],
+        ['images/axono_L1.png', 'images/axono_L2.png'],
+        ['images/symfony-learnning-L1.png', 'images/symfony-learnning-L2.png', 'images/symfony-learnning-L3.png'],
+    ];
+    let caroussel_index = [
+        [0, 3],
+        [0, 1],
+        [0, 2]
+    ];
     
-    console.log(window_width);
-
+    // if device screen is widther than 425px (mobile) then display only one picture and change each every said time
     if (window_width > 425) {
         $('#projects img:not(.caroussel)').toggleClass('menu-off');
+        setInterval(() => {
+            caroussel_index.forEach(e => {
+                if (e[0] + 1 > e[1]) {
+                    e[0] = 0;
+                }
+                else {
+                    e[0] = e[0] + 1;
+                }
+            });
+            img_element.each(e => {
+                console.log(img_element[e].src);
+               img_element[e].src = caroussel_array[e][caroussel_index[e][0]];
+            });
+        }, 5000);
+
     }
     else {
         $('.caroussel').toggleClass('menu-off');
