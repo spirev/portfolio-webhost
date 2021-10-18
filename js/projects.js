680567cb1ac213074fbd46093713cf4d3829dfbb
spirev <prospirev@gmail.com>
2021-10-18 15:53:41 +0200

    Page projets ok

--- js/projects.js
+++ js/projects.js
@@ -15,21 +15,33 @@ $(document).ready(function () {
     
     // if device screen is widther than 425px (mobile) then display only one picture and change each every said time
     if (window_width > 425) {
+        // display only one picture by project
         $('#projects img:not(.caroussel)').toggleClass('menu-off');
+      
+        // fade-out fade-in animation of projects pictures
         setInterval(() => {
-            caroussel_index.forEach(e => {
-                if (e[0] + 1 > e[1]) {
-                    e[0] = 0;
-                }
-                else {
-                    e[0] = e[0] + 1;
-                }
-            });
-            img_element.each(e => {
-                console.log(img_element[e].src);
-               img_element[e].src = caroussel_array[e][caroussel_index[e][0]];
-            });
+            img_element.toggleClass('project-fade');
+            // change picture on every timer length
+            setTimeout(() => {
+                caroussel_index.forEach(e => {
+                    if (e[0] + 1 > e[1]) {
+                        e[0] = 0;
+                    }
+                    else {
+                        e[0] = e[0] + 1;
+                    }
+                });
+                img_element.each(e => {
+                   img_element[e].src = caroussel_array[e][caroussel_index[e][0]];
+                });
+            }, 350);
+            
+            // delete 'project-fade' class from projects pictures to be able to apply it again on next iteration
+            setTimeout(() => {
+                img_element.toggleClass('project-fade');
+            }, 700)
         }, 5000);
+      
 
     }
     else {
